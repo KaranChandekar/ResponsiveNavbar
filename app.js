3bf69642b5729883621c50620a5e065466f477cb
Karan Chandekar <karanchandekar431@gmail.com>
2022-01-12 17:10:19 +0530

    add toggle function

--- app.js
+++ app.js
@@ -5,5 +5,12 @@
 // toggle - toggles class
 
 const navToggle = document.querySelector(".nav-toggle");
-
 const links = document.querySelector(".links");
+
+navToggle.addEventListener("click", function () {
+  if (links.classList.contains("show-links")) {
+    links.classList.remove("show-links");
+  } else {
+    links.classList.add("show-links");
+  }
+});
