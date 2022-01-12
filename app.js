4f45932da1473e3989d27e75e33a5fa71ce3c0a0
Karan Chandekar <karanchandekar431@gmail.com>
2022-01-12 16:59:20 +0530

    select elements

--- app.js
+++ app.js
@@ -3,3 +3,7 @@
 // add - add class
 // remove - remove class
 // toggle - toggles class
+
+const navToggle = document.querySelector(".nav-toggle");
+
+const links = document.querySelector(".links");
